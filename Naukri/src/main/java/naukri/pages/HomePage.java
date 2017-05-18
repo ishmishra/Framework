8d7df1cc944ba2a86a22875589fc3b3535d36599
mishrai <mishrai@LP-R9011A7T.HCLT.CORP.HCL.IN>
2017-05-18 09:34:49 +0530

    Drivers added

--- Naukri/src/main/java/naukri/pages/HomePage.java
+++ Naukri/src/main/java/naukri/pages/HomePage.java
@@ -3,11 +3,19 @@
  */
 package naukri.pages;
 
+import org.openqa.selenium.WebDriver;
+
 /**
  * @author IshM
  *
  */
 public class HomePage {
 
+	WebDriver driver;
+	
+	public static void main(String [] args){
+		
+		System.setProperty("webdriver.geckodriver.driver", value)
+	}
 	
 }
